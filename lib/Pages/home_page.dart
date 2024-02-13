6d47d7511bd5a902c16d651c2096d9122c0c36a9
manleKim <kk410910@naver.com>
2024-02-13 16:54:54 +0900

    [FEAT] 사진 찍은 후 메뉴판 화면으로 넘어가기 구현

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:goodeat_frontend/Pages/menu_board_page.dart';
 import 'package:goodeat_frontend/Pages/travel_lang_select_page.dart';
 import 'package:goodeat_frontend/controller/my_country_currency_controller.dart';
 import 'package:goodeat_frontend/controller/travel_controller.dart';
 import 'package:goodeat_frontend/pages/native_lang_select_page.dart';
 import 'package:goodeat_frontend/widgets/layout_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +19,18 @@ class _HomePageState extends State<HomePage> {
   final MyCountryCurrencyController firstController =
       Get.put(MyCountryCurrencyController());
   final TravelController secondController = Get.put(TravelController());
+
+  //image_picker
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final XFile? photo = await picker.pickImage(source: ImageSource.camera);
+
+    if (photo != null) {
+      //사진을 찍었다면
+      Get.to(const MenuBoardPage());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +65,7 @@ class _HomePageState extends State<HomePage> {
                 child: const Text('여행지 설정'),
               ),
               IconButton(
-                  onPressed: () {},
+                  onPressed: () => getImage(),
                   icon: const Icon(Icons.camera_alt_outlined)),
             ],
           ),
---
+++ lib/Pages/menu_board_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MenuBoardPage extends StatefulWidget {
+  const MenuBoardPage({super.key});
+
+  @override
+  State<MenuBoardPage> createState() => _MenuBoardPageState();
+}
+
+class _MenuBoardPageState extends State<MenuBoardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
