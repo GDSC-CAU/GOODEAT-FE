4949f82dc4b1037a198dfb2c528f59b0fe24e470
manleKim <kk410910@naver.com>
2024-02-14 16:51:07 +0900

    [FEAT] 찍은 사진을 보내고 메뉴판 정보를 받는 API 구현

--- lib/services/lang_currency.dart
+++ lib/services/lang_currency.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:goodeat_frontend/models/currency_model.dart';
+import 'package:goodeat_frontend/models/menu_model.dart';
 import 'package:goodeat_frontend/models/native_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,6 +9,7 @@ class ApiService {
   static final String baseURL = dotenv.env['BASE_URL']!;
   static const String language = 'language';
   static const String currency = 'currency';
+  static const String reconfigure = 'reconfigure';
 
   //나라 리스트 받기
   static Future<List<NativeModel>> getLanguages() async {
@@ -42,5 +44,35 @@ class ApiService {
         .toList();
 
     return currencyList;
+  }
+
+  //사진 보내고 메뉴 리스트 받기
+  static Future<List<MenuModel>> postPictureAndGetMenu(
+      {required String originLanguageName,
+      required String userLanguageName,
+      required String originCurrencyName,
+      required String userCurrencyName,
+      required String base64EncodedImage}) async {
+    List<MenuModel> menuList = [];
+    final body = {
+      "originLanguageName": originLanguageName,
+      "userLanguageName": userLanguageName,
+      "originCurrencyName": originCurrencyName,
+      "userCurrencyName": userCurrencyName,
+      "base64EncodedImage": base64EncodedImage
+    };
+    final header = {'Content-Type': 'application/json; charset=UTF-8'};
+    final url = Uri.parse('$baseURL/$reconfigure');
+    final response =
+        await http.post(url, body: jsonEncode(body), headers: header);
+
+    if (response.statusCode != 200) {
+      const String errorMessage = '메뉴판 리스트 받아오기를 실패하였습니다.';
+      throw Exception(errorMessage);
+    }
+    final List<dynamic> responseList =
+        jsonDecode(utf8.decode(response.bodyBytes));
+    menuList = responseList.map((menu) => MenuModel.fromJson(menu)).toList();
+    return menuList;
   }
 }
