4d54089142b7138ef04d703668588bf3896f0bdc
manleKim <kk410910@naver.com>
2024-02-14 17:16:40 +0900

    [FEAT] TravelController에 여행지 사용언어와 화폐 추가

--- lib/controller/travel_controller.dart
+++ lib/controller/travel_controller.dart
@@ -2,11 +2,17 @@ import 'package:get/get.dart';
 
 class TravelController extends GetxController {
   String _travel = 'null';
+  String _travelLanguage = 'null';
+  String _travelCurrency = 'null';
 
   get travel => _travel;
+  get travelLanguage => _travelLanguage;
+  get travelCurrency => _travelCurrency;
 
-  void modify(String travel) {
+  void modify(String travel, String travelLanguage, String travelCurrency) {
     _travel = travel;
+    _travelLanguage = travelLanguage;
+    _travelCurrency = travelCurrency;
     update();
   }
 }
