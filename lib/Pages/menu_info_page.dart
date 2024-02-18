c0d6423b5170fa0388a80c809a79c10c139a0017
manleKim <kk410910@naver.com>
2024-02-18 16:34:11 +0900

    [FEAT] 메뉴 Info 페이지 UI 뿌리기

--- lib/Pages/menu_info_page.dart
+++ lib/Pages/menu_info_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:footer/footer.dart';
-import 'package:footer/footer_view.dart';
 import 'package:get/get.dart';
 import 'package:goodeat_frontend/models/menu_model.dart';
 import 'package:goodeat_frontend/widgets/text_widgets.dart';
@@ -20,19 +18,100 @@ class _MenuInfoPageState extends State<MenuInfoPage> {
       appBar: AppBar(
         title: const AppBarText(text: 'Menu Detail'),
       ),
-      body: FooterView(
-        footer: Footer(
-            backgroundColor: Colors.white,
-            child: OutlinedButton(
-              onPressed: () => Get.back(),
-              child: const Text('Add to Cart'),
-            )),
+      body: Column(
         children: [
-          Image.network(
-            widget.menu.imageUrl,
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
+          Expanded(
+            child: Column(
+              children: [
+                //음식 사진
+                Image.network(
+                  widget.menu.imageUrl,
+                  height: 150,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+                //음식 이름, 가격
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TitleText(text: widget.menu.userMenuName),
+                          CaptionText(text: widget.menu.originMenuName),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const MainText(text: 'Price'),
+                              MainText(text: '${widget.menu.userPrice}')
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const MainText(text: 'Local Price'),
+                              MainText(text: '${widget.menu.originPrice}')
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    //음식 설명 파트
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 24),
+                      child: Container(
+                        alignment: Alignment.topLeft,
+                        child: CaptionText(text: widget.menu.description),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    //Quantity 파트
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          MainText(text: 'Quantity'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          //카트 추가 버튼
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: GestureDetector(
+              onTap: () => Get.back(),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 48,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFF545F70),
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(width: 2, color: Color(0xFF545F70)),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MainText(text: 'Add to Cart'),
+                    SizedBox(width: 10),
+                    Icon(Icons.shopping_cart_outlined)
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/widgets/text_widgets.dart
+++ lib/widgets/text_widgets.dart
@@ -20,3 +20,41 @@ class HeadingText extends Text {
             text,
             style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold));
 }
+
+class TitleText extends Text {
+  final String text;
+
+  const TitleText({
+    Key? key,
+    required this.text,
+  }) : super(
+            key: key,
+            text,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold));
+}
+
+class CaptionText extends Text {
+  final String text;
+
+  const CaptionText({
+    Key? key,
+    required this.text,
+  }) : super(
+            key: key,
+            text,
+            style:
+                const TextStyle(fontSize: 14, fontWeight: FontWeight.normal));
+}
+
+class MainText extends Text {
+  final String text;
+
+  const MainText({
+    Key? key,
+    required this.text,
+  }) : super(
+            key: key,
+            text,
+            style:
+                const TextStyle(fontSize: 16, fontWeight: FontWeight.normal));
+}
